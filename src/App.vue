1b6af269f2ae78d546788f1ebf8c36e2d636f583
Elena Vasileva <milena-iron@yandex.ru>
2025-06-11 19:12:28 +0700

    feat: добавить структуру файлов

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <header>
+    
+  </header>
+
+  <main>
+
+  </main>
+
+</template>
+
+
+<script setup>
+
+</script>
+
+
+<style scoped>
+
+
+/* @media (min-width: 1024px) {
+} */
+</style>
