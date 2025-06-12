a6e94f322c71d1e16a2a6ba4ef1413aa889d3fc8
Elena Vasileva <milena-iron@yandex.ru>
2025-06-12 20:41:32 +0700

    feat: banner, style: header, base

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,8 +1,8 @@
 <template>
     <section className="banner">
         <div className="banner__container container">
-            <h1 className="banner__title">Книги, которые вознесут вас на Олимп</h1>
-            <a className="banner__link link" href="#">Узнать больше</a>
+            <h1 className="banner__title color-accent">Книги, которые вознесут вас на Олимп</h1>
+            <a className="banner__link link text text_l" href="#">Узнать больше</a>
         </div>
     </section>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -19,10 +19,13 @@
 
         <div id="header__burger-menu" className="header__burger-menu" uk-open uk-modal-full uk-modal>
              <div className="header__burger-menu-content uk-modal-dialog uk-modal-body">
-                <ul className="header__burger-menu-items">
-                    <li className="header__burger-menu-item"><a className="header__burger-menu-link link" href="#">Главная</a></li>
-                    <li className="header__burger-menu-item"><a className="header__burger-menu-link link" href="#">Каталог</a></li>
-                    <li className="header__burger-menu-item"><a className="header__burger-menu-link link" href="#">Корзина</a></li>
+                <a href="#header__burger-menu" className="header__burger header__burger_active link" uk-toggle>
+                    <img src="../assets/images/icons/header-arrow.svg" alt="Стрелка" className="header__burger-icon" uk-svg />
+                </a>
+            <ul className="header__burger-menu-items">
+                    <li className="header__burger-menu-item"><a className="header__burger-menu-link link link_accent text text_m" href="/">Главная</a></li>
+                    <li className="header__burger-menu-item"><a className="header__burger-menu-link link link_accent text text_m" href="/">Каталог</a></li>
+                    <li className="header__burger-menu-item"><a className="header__burger-menu-link link link_accent text text_m" href="/">Корзина</a></li>
                 </ul>
             </div>
         </div>
