4d944e3a0c14786220442377a5b43add35b98b34
Elena Vasileva <milena-iron@yandex.ru>
2025-06-13 11:07:18 +0700

    feat: description

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -1,3 +1,39 @@
 <template>
-    
+  <section className="description">
+    <div className="description__container container">
+
+      <div className="description__cell description__cell_with_image" :style="imageStyleBooks"></div>
+
+      <div className="description__cell description__cell_with_text">
+        <h2 className="description__cell-title color-accent">О нас</h2>
+        <p className="description__cell-text color-accent">Книжное наследие от мыслителей Древней Греции и до людей современности. Это не просто магазин, а настоящий клуб любителей книги, который объединяет в своем культурном пространстве людей самых разных интересов.</p>
+      </div>
+
+      <div className="description__cell description__cell_with_text">
+        <h2 className="description__cell-title color-accent">Гармония</h2>
+        <p className="description__cell-text color-accent">Наш проект «Гармония» объединяет читателей и авторов в реальности и в интернете. Каждую неделю проводятся встречи онлайн и вживую. Из наших регулярных рубрик вы узнаете много нового о книгах и писателях.</p>
+      </div>
+
+      <div className="description__cell description__cell_with_image" :style="imageStyleStatue"></div>
+
+    </div>
+  </section>
 </template>
+
+<script>
+import imageBooks from '../assets/images/background/description-books.jpg';
+import imageStatue from '../assets/images/background/description-statue.jpg';
+
+export default {
+  data() {
+    return {
+      imageStyleBooks: {
+        backgroundImage: `url(${imageBooks})`,
+      },
+      imageStyleStatue: {
+        backgroundImage: `url(${imageStatue})`,
+      },
+    };
+  },
+};
+</script>
--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,3 +1,21 @@
 <template>
-    
+    <!-- <section id="slider" class="slider">
+        <div class="slider__container container">
+            <div class="slider__content uk-position-relative uk-visible-toggle uk-light" tabindex="0"
+                uk-slider="finite: false">
+                <div
+                    class="slider__items uk-slider-items uk-child-width-1-1 uk-child-width-1-3@m uk-child-width-1-2@s">
+                
+                    <div class="slider__item uk-slider-item slider__item">
+                        <div class="slider__item-wrapper slider__item-wrapper">
+                            
+                        </div>
+                    </div>
+                </div>
+                 навигационные точки 
+                <ul class="slider__nav uk-slider-nav uk-dotnav uk-flex-center uk-margin">
+                </ul>
+            </div>
+        </div>
+    </section> -->
 </template>
